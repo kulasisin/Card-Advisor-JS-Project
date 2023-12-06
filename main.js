6a889b90d3f8528839f8bbb3b468b1e0228438c0
kulasisin <107702039@mail2.nccu.tw>
2023-12-07 03:06:08 +0800

    second

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
-
+console.log("Hello world!");
 // added by angela
 document.addEventListener('DOMContentLoaded', function () {
     const mobileMenuButton = document.getElementById('mobile-menu');
